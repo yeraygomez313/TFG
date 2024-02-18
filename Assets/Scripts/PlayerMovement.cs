1cac9d1e8ca2e4624ddc457c20fecf3e5a361ef1
yeraygomez313 <yerygominola82@gmail.com>
2024-02-18 21:12:43 +0100

    Dash and invisibility
    The dash has been implemented. I have also added an hability that makes the player a little bit transparent.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,19 +9,27 @@ public class PlayerMovement : MonoBehaviour
     public float dashForce = 10f; // Dash force
     private Rigidbody2D rb;
     private bool isGrounded;
+    private bool isDashing = false;
+    private float elapsedTime = 0f;
+    private float dashingTime = 0.3f;
+    private SpriteRenderer playerSprite;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+
         // Horizontal movement
         float moveInput = Input.GetAxis("Horizontal");
         
         // This has an error, it overrides the force and disables the function of dashing
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        if (!isDashing)
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
         // Jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -29,11 +37,51 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        // Dash
-        if (Input.GetKeyDown(KeyCode.E) && !isGrounded)
+        // Dash E
+        if (Input.GetKeyDown(KeyCode.E) && !isGrounded && elapsedTime > 1f)
         {
             //moveSpeed = dashForce;
-            rb.velocity = new Vector2(dashForce, rb.velocity.y);
+            
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(dashForce, 0f);
+            isDashing = true;
+            elapsedTime = 0f;
+        }
+
+        // Dash Q
+        if (Input.GetKeyDown(KeyCode.Q) && !isGrounded && elapsedTime > 1f)
+        {
+            //moveSpeed = dashForce;
+            
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(-dashForce, 0f);
+            isDashing = true;
+            elapsedTime = 0f;
+        }
+
+        // It is needed to make a progressive transition, a cooldown and blocking the controls.
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded && rb.velocity.x == 0f && elapsedTime > 3f)
+        {
+            Color newColor = playerSprite.color;
+            newColor.a = 0.3f;
+            playerSprite.color = newColor;
+            elapsedTime = 0f;
+        }
+
+        // Changes the opacity to the original opacity
+        if (elapsedTime > 3f)
+        {
+            Color newColor = playerSprite.color;
+            newColor.a = 1f;
+            playerSprite.color = newColor;
+        }
+
+
+        // Stops the dash and allows the player to use the rest of the inputs
+        if (elapsedTime > dashingTime)
+        {
+            rb.gravityScale = 9.8f;
+            isDashing = false;
         }
     }
 
