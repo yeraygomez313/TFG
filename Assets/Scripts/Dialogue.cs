b6c4067d7c0052e6dd9a2f12db603fdce04b2719
yeraygomez313 <yerygominola82@gmail.com>
2024-03-20 21:03:24 +0100

    TextManager

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] private GameObject canDialogue;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
+    private float typingTime = 0.05f;
+
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetButtonDown("Fire1"))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        canDialogue.SetActive(false);
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            canDialogue.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            canDialogue.SetActive(false);
+        }
+    }
+}
