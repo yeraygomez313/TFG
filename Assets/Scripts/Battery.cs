038f6c180fcd91eafa669a22ad05d695b59873d6
yeraygomez313 <yerygominola82@gmail.com>
2024-06-03 15:26:21 +0200

    Batería añadida
    He añadido las baterías. Tienen una idle animation.

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    [SerializeField] private float amplitude = 0.5f; // Amplitud del movimiento (qué tan alto/sube bajo)
+    [SerializeField] private float speed = 1.0f; // Velocidad del movimiento
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        // Guarda la posición inicial del GameObject
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Calcula la posición vertical utilizando una función sinusoidal
+        float yPos = startPosition.y + amplitude * Mathf.Sin(speed * Time.time);
+
+        // Actualiza la posición del GameObject
+        transform.position = new Vector3(startPosition.x, yPos, startPosition.z);
+    }
+}
