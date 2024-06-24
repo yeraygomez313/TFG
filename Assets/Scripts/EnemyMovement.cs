52920c45b1540fccc5a895f9b5a3ed7b0f372a05
yeraygomez313 <yerygominola82@gmail.com>
2024-06-24 18:31:03 +0200

    Bad ending y más sonidos nivel 2
    He hecho el bad ending y he añadido más sonidos al nivel 2

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Referencia al objeto del jugador
+    public Transform player;
+    // Velocidad de movimiento del enemigo
+    public float speed = 5f;
+
+    void Update()
+    {
+        // Calcular la dirección hacia el jugador
+        Vector3 direction = (player.position - transform.position).normalized;
+
+        // Mover el enemigo hacia el jugador
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Comprobar si el enemigo ha entrado en el área de la luz
+        if (collision.CompareTag("Player"))
+        {
+            print("te mato");
+        }
+    }
+}
--- Assets/Scripts/Trone.cs
+++ Assets/Scripts/Trone.cs
@@ -6,6 +6,8 @@ public class Trone : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
 
+    private bool playOnce = true;
+
     void Update()
     {
 
@@ -13,8 +15,9 @@ public class Trone : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && playOnce)
         {
+            playOnce = false;
             panel.SetActive(true);
             RuneDialogue.showRune = true;
         }
